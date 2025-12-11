3f07bc3de709b3b54a82b73cb8be4ab005417776
ifti-github <minhajuddinifti2094944@gmail.com>
2025-12-11 12:12:30 +0600

    set up environment variable and connected to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//wLp5sr5DconCsAUM
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@artify.ndgtchi.mongodb.net/?appName=Artify`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.use(cors())
 app.use(express.json())
